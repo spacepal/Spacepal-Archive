ce3df1d9933cfa2e569965b524de8595fec84d74
hedhyw <hedhyw@yahoo.com>
2018-06-22 11:59:31 +0300

    AI microservice hello world

---
+++ Microservices/deps_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+go get -d -u github.com/golang/dep
+cd $(go env GOPATH)/src/github.com/golang/dep
+DEP_LATEST=$(git describe --abbrev=0 --tags)
+git checkout $DEP_LATEST
+go install -ldflags="-X main.version=$DEP_LATEST" ./cmd/dep
+git checkout master
