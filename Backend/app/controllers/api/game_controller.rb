371bf1f067dff4c0fef53cec90b228da1e150c12
M-Nick <sunty1@mail.ru>
2018-06-29 00:03:50 +0300

    Game#index -> games:[], count:#

--- Backend/app/controllers/api/game_controller.rb
+++ Backend/app/controllers/api/game_controller.rb
@@ -1,7 +1,7 @@
 class Api::GameController < ApplicationController
   def index
     arr = Game.all :offset => params[:offset], :limit => params[:limit]
-    render :json => { games: arr }
+    render :json => { games: arr, count = $redis.zcard "game:ids" }
   end
 
   def create
