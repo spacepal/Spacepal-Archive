978ec3970bd9ac9a213b3811d8ff25c650055a91
M-Nick <sunty1@mail.ru>
2018-06-29 08:59:06 +0300

    Allow cross-origin requests

--- Backend/config/application.rb
+++ Backend/config/application.rb
@@ -23,7 +23,12 @@ module Backend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :delete, :options, :put]
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
