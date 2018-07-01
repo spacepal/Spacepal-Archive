0071a6c23e84f4eb1282b8cb907bafaaef99b669
hedhyw <hedhyw@yahoo.com>
2018-07-01 20:21:12 +0300

    Create switchbox

---
+++ Frontend/src/components/SwitchBox.vue
@@ -0,0 +1,24 @@
+<template>
+  <label class="switch" :title="title">
+    <p class="label-text">
+      {{ label }}
+    </p>
+    <input type="checkbox" v-model="value" v-on:input="$emit('input', $event.target.checked)">
+    <div class="slider"></div>
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'SwitchBox',
+  props: {
+    label: String,
+    title: String,
+    value: Boolean
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../css/components/_switch_box.scss';
+</style>
