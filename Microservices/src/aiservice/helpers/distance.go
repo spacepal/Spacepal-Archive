c19f8137b12977a1452b84583b63d5bd3db90278
hedhyw <hedhyw@yahoo.com>
2018-07-08 21:02:45 +0300

    Review distance helper

--- Microservices/src/aiservice/helpers/distance.go
+++ Microservices/src/aiservice/helpers/distance.go
@@ -17,93 +17,81 @@ func (m mapSize) isValid() bool {
 	return m.width > 0 && m.height > 0
 }
 
+// DistanceSurface represents a map of offset distances relative to cell
+type DistanceSurface struct {
+	mapSize mapSize
+	cell    cell
+	surface map[int]int
+}
+
+// NewDistanceSurface initializes DistanceSurface relative to cell
+func NewDistanceSurface(cellID, mapWidth, mapHeight int) *DistanceSurface {
+	var surface DistanceSurface
+	surface.mapSize = mapSize{width: mapWidth, height: mapHeight}
+	if !surface.mapSize.isValid() {
+		return nil
+	}
+	if cellID < 1 || cellID > surface.mapSize.lastCellID() {
+		return nil
+	}
+	surface.surface = make(map[int]int)
+	surface.cell = cell{cellID}
+	var firstCube = surface.cell.cubeCoord(surface.mapSize)
+	for i := 1; i <= surface.mapSize.lastCellID(); i++ {
+		var secondCube = cell{i}.cubeCoord(surface.mapSize)
+		surface.surface[i] = secondCube.distanceTo(firstCube)
+	}
+	return &surface
+}
+
+// DistanceTo returns shortest distance to given cell
+func (s DistanceSurface) DistanceTo(cell int) int {
+	if cell < 1 || cell > s.mapSize.lastCellID() {
+		return -1
+	}
+	return s.surface[cell]
+}
+
 type cell struct {
-	x       int
-	y       int
-	cellID  int
-	mapSize *mapSize
+	id int
 }
 
-func newCellByID(cellID int, mapSize *mapSize) *cell {
-	if !mapSize.isValid() || cellID < 1 ||
-		cellID > mapSize.lastCellID() {
-		return nil
-	}
-	return &cell{
-		x:       (cellID - 1) % mapSize.width,
-		y:       int(math.Ceil(float64(cellID-1) / float64(mapSize.width))),
-		mapSize: mapSize,
-		cellID:  cellID,
+func (c cell) offsetCoord(mapSize mapSize) offsetCoord {
+	return offsetCoord{
+		x: (c.id - 1) % mapSize.width,
+		y: int(math.Floor(float64(c.id-1) / float64(mapSize.width))),
 	}
 }
 
-func newCellByPos(x, y int, mapSize *mapSize) *cell {
-	if !mapSize.isValid() || x < 0 || y < 0 ||
-		x >= mapSize.width || y >= mapSize.height {
-		return nil
-	}
-	return &cell{
-		x:       x,
-		y:       y,
-		mapSize: mapSize,
-		cellID:  x + y*mapSize.width + 1,
-	}
+func (c cell) cubeCoord(mapSize mapSize) cubeCoord {
+	return c.offsetCoord(mapSize).cubeCoord()
 }
 
-// safePush append given cell to a slice of cells
-// if this cell is not equal to nil
-func safePush(to *[]*cell, el *cell) {
-	if el != nil {
-		*to = append(*to, el)
-	}
+type cubeCoord struct {
+	x int
+	y int
+	z int
 }
 
-func (c cell) neighbours() []*cell {
-	var cells = make([]*cell, 0)
-	safePush(&cells, newCellByPos(c.x-1, c.y, c.mapSize))
-	safePush(&cells, newCellByPos(c.x+1, c.y, c.mapSize))
-	safePush(&cells, newCellByPos(c.x, c.y-1, c.mapSize))
-	safePush(&cells, newCellByPos(c.x, c.y+1, c.mapSize))
-	var delta = -1 // for odd
-	if c.cellID%2 == 0 {
-		delta = +1 // for even
-	}
-	safePush(&cells, newCellByPos(c.x-1, c.y+delta, c.mapSize))
-	safePush(&cells, newCellByPos(c.x+1, c.y+delta, c.mapSize))
-	return cells
+func (first cubeCoord) distanceTo(second cubeCoord) int {
+	var dx = math.Abs(float64(first.x - second.x))
+	var dy = math.Abs(float64(first.y - second.y))
+	var dz = math.Abs(float64(first.z - second.z))
+	return int((dx + dy + dz) / 2.0)
 }
 
-// CalcDistance calculates map of distances from specific cell to each other
-// cellID  starts  with 1
-func CalcDistance(from, mapWidth, mapHeight int) (hexSurface map[int]int) {
-	var mapSize = &mapSize{
-		width:  mapWidth,
-		height: mapHeight,
-	}
-	if !mapSize.isValid() {
-		return nil
-	}
+// odd columns by +0.5 row
+type offsetCoord struct {
+	x int
+	y int
+}
 
-	var fromCell = newCellByID(from, mapSize)
-	if fromCell == nil {
-		return nil
+func (c offsetCoord) cubeCoord() cubeCoord {
+	var oddFactor = c.x & 1
+	var z = c.y - (c.x-oddFactor)/2
+	return cubeCoord{
+		x: c.x,
+		z: z,
+		y: -c.x - z,
 	}
-
-	hexSurface = make(map[int]int)
-	var pendingCells = []*cell{newCellByID(from, mapSize)}
-	var tmpCells = make([]*cell, 0)
-	var step = 0
-	for len(pendingCells) > 0 {
-		for _, cell := range pendingCells {
-			if _, ok := hexSurface[cell.cellID]; ok {
-				continue
-			}
-			hexSurface[cell.cellID] = step
-			tmpCells = append(tmpCells, cell.neighbours()...)
-		}
-		pendingCells = tmpCells
-		tmpCells = make([]*cell, 0)
-		step++
-	}
-	return
 }
--- Microservices/src/aiservice/helpers/distance_test.go
+++ Microservices/src/aiservice/helpers/distance_test.go
@@ -1,39 +1,115 @@
 package helpers
 
-import "testing"
+import (
+	"testing"
+)
 
-var distanceMainCases = []struct {
-	id        int
-	from      int
-	to        int
-	mapWidth  int
-	mapHeight int
-	want      int
+var coordConvCases = []struct {
+	id          int
+	offsetCoord offsetCoord
+	cubeCoord   cubeCoord
 }{
 	{
-		id:        1,
-		from:      3,
-		to:        4,
-		mapWidth:  0,
-		mapHeight: 3,
-		want:      -1,
+		id:          1,
+		offsetCoord: offsetCoord{x: 0, y: 0},
+		cubeCoord:   cubeCoord{x: 0, z: 0, y: 0},
 	},
 	{
-		id:        2,
-		from:      0,
-		to:        10,
-		mapWidth:  9,
-		mapHeight: 9,
-		want:      -1,
+		id:          2,
+		offsetCoord: offsetCoord{x: -2, y: -2},
+		cubeCoord:   cubeCoord{x: -2, z: -1, y: +3},
 	},
 	{
-		id:        3,
-		from:      1,
-		to:        12,
-		mapWidth:  4,
-		mapHeight: 3,
-		want:      4,
+		id:          3,
+		offsetCoord: offsetCoord{x: +2, y: +1},
+		cubeCoord:   cubeCoord{x: +2, z: 0, y: -2},
 	},
+	{
+		id:          4,
+		offsetCoord: offsetCoord{x: +2, y: -1},
+		cubeCoord:   cubeCoord{x: +2, z: -2, y: 0},
+	},
+}
+
+func TestCoordinateConversions(t *testing.T) {
+	for _, c := range coordConvCases {
+		var get = c.offsetCoord.cubeCoord()
+		if c.offsetCoord.cubeCoord() != c.cubeCoord {
+			t.Error("Invalid case: ", c.id, "; want: ", c.cubeCoord, ", get: ", get)
+		}
+	}
+}
+
+func TestCubeCoordinatesDistance(t *testing.T) {
+	var first = cubeCoord{x: -1, y: +1, z: 0}
+	var second = cubeCoord{x: -4, y: +2, z: +2}
+	var want = 3
+	var get = first.distanceTo(second)
+	if want != get {
+		t.Error("Invalid distance; want: ", want, ", get: ", get)
+	}
+}
+
+var mapSizeValidationCases = []struct {
+	id      int
+	valid   bool
+	mapSize mapSize
+}{
+	{id: 1, valid: true, mapSize: mapSize{width: 10, height: 10}},
+	{id: 2, valid: true, mapSize: mapSize{width: 100, height: 1}},
+	{id: 3, valid: false, mapSize: mapSize{width: -10, height: 10}},
+	{id: 4, valid: false, mapSize: mapSize{width: 10, height: -10}},
+	{id: 5, valid: false, mapSize: mapSize{width: -10, height: -101}},
+}
+
+func TestMapSizeValidation(t *testing.T) {
+	for _, c := range mapSizeValidationCases {
+		get := c.mapSize.isValid()
+		if c.valid != get {
+			t.Error("Invalid case: ", c.id, "; want: ", c.valid, ", get: ", get)
+		}
+	}
+}
+
+var cellConvCases = []struct {
+	id      int
+	mapSize mapSize
+	cell    cell
+	coord   offsetCoord
+}{
+	{
+		id:      1,
+		mapSize: mapSize{width: 10, height: 10},
+		cell:    cell{1},
+		coord:   offsetCoord{x: 0, y: 0},
+	},
+	{
+		id:      2,
+		mapSize: mapSize{width: 4, height: 3},
+		cell:    cell{5},
+		coord:   offsetCoord{x: 0, y: 1},
+	},
+	{
+		id:      3,
+		mapSize: mapSize{width: 4, height: 3},
+		cell:    cell{10},
+		coord:   offsetCoord{x: 1, y: 2},
+	},
+	{
+		id:      4,
+		mapSize: mapSize{width: 4, height: 3},
+		cell:    cell{12},
+		coord:   offsetCoord{x: 3, y: 2},
+	},
+}
+
+func TestCellConversions(t *testing.T) {
+	for _, c := range cellConvCases {
+		get := c.cell.offsetCoord(c.mapSize)
+		if c.coord != get {
+			t.Error("Invalid case: ", c.id, "; want: ", c.coord, ", get: ", get)
+		}
+	}
 }
 
 var distanceSurfaceCase = struct {
@@ -61,28 +137,25 @@ var distanceSurfaceCase = struct {
 	},
 }
 
-func TestDistance(t *testing.T) {
-	for _, c := range distanceMainCases {
-		var get = -1
-		surface := CalcDistance(c.from, c.mapWidth, c.mapHeight)
-		if surface != nil {
-			if toDistance, ok := surface[c.to]; ok {
-				get = toDistance
-			}
-		}
-		if get != c.want {
-			t.Error("Invalid case: ", c.id, "; want: ", c.want, ", get: ", get)
-		}
-	}
-}
-
 func TestDistanceSurface(t *testing.T) {
+	if NewDistanceSurface(1, -10, 10) != nil {
+		t.Fatal("NewDistanceSurface(1, -10, 10) != nil")
+	}
+	if NewDistanceSurface(-1, 10, 10) != nil {
+		t.Fatal("NewDistanceSurface(-1, 10, 10) != nil")
+	}
+	if NewDistanceSurface(-1, 10, 10) != nil {
+		t.Fatal("NewDistanceSurface(-1, 10, 10) != nil")
+	}
 	var c = distanceSurfaceCase
-	surface := CalcDistance(c.from, c.mapWidth, c.mapHeight)
-	for cellID, want := range c.to {
-		get := surface[cellID]
+	var s = NewDistanceSurface(c.from, c.mapWidth, c.mapHeight)
+	if s == nil {
+		t.Fatal("s == nil")
+	}
+	for to, want := range c.to {
+		get := s.DistanceTo(to)
 		if get != want {
-			t.Error("Invalid distance, want: ", want, ", get: ", get)
+			t.Error("Invalid distance to: ", to, "; want: ", want, ", get: ", get)
 		}
 	}
 }
