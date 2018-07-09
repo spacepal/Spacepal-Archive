eda136958bc812fadb2a987d0aa2c3b6187f8633
hedhyw <hedhyw@yahoo.com>
2018-07-09 17:47:31 +0300

    Add PlanetHelper test

---
+++ Microservices/src/aiservice/helpers/planets_test.go
@@ -0,0 +1,57 @@
+package helpers
+
+import (
+	"aiservice/constants"
+	"aiservice/model"
+	"aiservice/model/imodel"
+	"testing"
+)
+
+func TestPlanets(t *testing.T) {
+	var ownerID = 1
+	var planets = []imodel.PlanetGetter{
+		model.Planet{OwnerID: 3},
+		model.Planet{OwnerID: 1},
+		model.Planet{OwnerID: 1},
+		model.Planet{OwnerID: 2},
+		model.Planet{OwnerID: constants.NeutralOwnerID},
+	}
+	selfPlanetsCount := 2
+	foreignPlanetsCount := 3
+
+	planetsHelper := NewPlanets(planets, ownerID)
+	if planetsHelper == nil {
+		t.Fatal("planetsHelper == nil")
+	}
+
+	if len(planetsHelper.Foreign()) != foreignPlanetsCount {
+		t.Errorf("Invalid foreign planets count. Want: %d, get: %d",
+			foreignPlanetsCount, len(planetsHelper.Foreign()))
+	}
+	if len(planetsHelper.Self()) != selfPlanetsCount {
+		t.Errorf("Invalid self planets count. Want: %d, get: %d",
+			selfPlanetsCount, len(planetsHelper.Self()))
+	}
+	for _, p := range planetsHelper.Self() {
+		if p.Owner() != ownerID {
+			t.Errorf("Self planet do not belongs to player! PlanetID, want: %d, get: %d",
+				ownerID, p.Owner())
+		}
+	}
+	for _, p := range planetsHelper.Foreign() {
+		if p.Owner() == ownerID {
+			t.Errorf("Foreign planet belongs to current player! PlanetID, get: %d",
+				p.Owner())
+		}
+	}
+
+	planetsHelper = NewPlanets([]imodel.PlanetGetter{}, ownerID)
+	if len(planetsHelper.Foreign()) != 0 {
+		t.Log("Foreign planet, for empty collection, exists")
+		t.Errorf("len(planetsHelper.Foreign()) != 0")
+	}
+	if len(planetsHelper.Self()) != 0 {
+		t.Log("Self planet, for empty collection, exists")
+		t.Errorf("len(planetsHelper.Self()) != 0")
+	}
+}
