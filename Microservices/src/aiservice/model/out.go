2836932a489650e323ded8d5296df420ef81de0d
hedhyw <hedhyw@yahoo.com>
2018-07-09 15:38:45 +0300

    Add TasksOut

--- Microservices/src/aiservice/model/out.go
+++ Microservices/src/aiservice/model/out.go
@@ -1,7 +1,19 @@
 package model
 
+import (
+	"aiservice/model/imodel"
+)
+
 // AINamesOut represents a model of response with all registered AI names
 type AINamesOut struct {
 	All   []string `json:"all"`
 	Count int      `json:"count"`
+}
+type TasksOut struct {
+	Players []PlayerOut `json:"players"`
+}
+
+type PlayerOut struct {
+	Player int                 `json:"id"`
+	Tasks  []imodel.TaskGetter `json:"tasks"`
 }
