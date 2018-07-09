61eb08c2a0a1571e09cabf4f5638fa5286b2d10d
hedhyw <hedhyw@yahoo.com>
2018-07-09 15:56:44 +0300

    Implement AIPlayer interface

--- Microservices/src/aiservice/model/aiplayer.go
+++ Microservices/src/aiservice/model/aiplayer.go
@@ -8,15 +8,25 @@ import (
 
 // AIPlayer represents main information about computer-player
 type AIPlayer struct {
-	Name   string `json:"aiName"`
-	Player int    `json:"playerID"`
+	AIName   string `json:"aiName"`
+	PlayerID int    `json:"playerID"`
+}
+
+// Name returns the name of the artificial intelligence
+func (ai AIPlayer) Name() string {
+	return ai.AIName
+}
+
+// Player returns the AI player ID
+func (ai AIPlayer) Player() int {
+	return ai.PlayerID
 }
 
 // Check validates AIPlayer model
 func (ai AIPlayer) check(manager iai.ManagerChecker) error {
-	if !manager.Check(ai.Name) {
+	if !manager.Check(ai.AIName) {
 		return errors.New(
-			fmt.Sprint("Invalid AI name. PlayerID:", ai.Player))
+			fmt.Sprint("Invalid AI name. PlayerID:", ai.PlayerID))
 	}
 	return nil
 }
