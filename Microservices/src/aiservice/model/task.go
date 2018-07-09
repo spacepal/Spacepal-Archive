b9c4b4abd217a5433344fd6a2fafabad429cdcd7
hedhyw <hedhyw@yahoo.com>
2018-07-09 11:36:52 +0300

    Add Task factory method

--- Microservices/src/aiservice/model/task.go
+++ Microservices/src/aiservice/model/task.go
@@ -1,10 +1,19 @@
 package model
 
+import (
+	"aiservice/model/imodel"
+)
+
 // Task represents an action per turn
 type Task struct {
 	FromID     int `json:"from"`
 	ToID       int `json:"to"`
 	ShipsCount int `json:"count"`
+}
+
+// NewTask is the Task-factory method
+func NewTask(from, to, ships int) imodel.TaskGetter {
+	return &Task{FromID: from, ToID: to, ShipsCount: ships}
 }
 
 // From returns id of planet from where ships are sent
