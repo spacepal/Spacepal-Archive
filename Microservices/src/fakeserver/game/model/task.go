55ca9447c8698020428f8542f9593878385413dc
hedhyw <hedhyw@yahoo.com>
2018-07-10 17:46:45 +0300

    Add task validation

--- Microservices/src/fakeserver/game/model/task.go
+++ Microservices/src/fakeserver/game/model/task.go
@@ -1,5 +1,7 @@
 package model
 
+import "errors"
+
 // Task is a
 type Task struct {
 	Player int `json:"playerID"`
@@ -7,4 +9,21 @@ type Task struct {
 	To     int `json:"to"`
 	Count  int `json:"count"`
 	Steps  int `json:"-"`
+}
+
+// Check validates Task
+func (t *Task) Check() error {
+	if t.From < 0 {
+		return errors.New("Task: From id is negative")
+	}
+	if t.To < 0 {
+		return errors.New("Task: To id is negative")
+	}
+	if t.Count < 0 {
+		return errors.New("Task: Ships count is negative")
+	}
+	if t.Steps <= 0 {
+		return errors.New("Task: Steps count must be greater than 0")
+	}
+	return nil
 }
