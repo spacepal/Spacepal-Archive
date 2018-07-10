d262fda92af89fd26c52bb58640ffa772f3e174c
hedhyw <hedhyw@yahoo.com>
2018-07-10 17:28:16 +0300

    Use PickRandom

--- Microservices/src/fakeserver/game/player.go
+++ Microservices/src/fakeserver/game/player.go
@@ -1,9 +1,5 @@
 package game
 
-import (
-	"errors"
-)
-
 // Player is a model of game AI member
 type player struct {
 	PlayerID int    `json:"playerID"`
@@ -12,18 +8,15 @@ type player struct {
 
 // genPlayers generates a list of random players
 func (g *Game) genPlayers() error {
-	names, err := getAINames()
+	aiNames, err := newAiNames()
 	if err != nil {
 		return err
-	}
-	if len(names) == 0 {
-		return errors.New("AI not found")
 	}
 	g.players = make([]player, g.params.PlayersCount)
 	for i := 0; i < g.params.PlayersCount; i++ {
 		g.players[i] = player{
 			PlayerID: i + 1,
-			AIName:   "name",
+			AIName:   aiNames.PickRandom(),
 		}
 	}
 	return nil
