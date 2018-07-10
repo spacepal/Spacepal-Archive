dbebae44a2a97bde4b9eda35afbe1fcb0652a978
hedhyw <hedhyw@yahoo.com>
2018-07-10 21:40:51 +0300

    Cosmetic change

--- Microservices/src/aiservice/ai/base_test.go
+++ Microservices/src/aiservice/ai/base_test.go
@@ -83,7 +83,7 @@ func TestBase(t *testing.T) {
 	if tasks == nil {
 		t.Fatal("The Base AI doesn't make a turn")
 	}
-	if len(tasks) != len(planets.Self())*2 { // *2 - attach and redistribution
+	if len(tasks) != len(planets.Self())*2 { // *2 - attack and redistribution
 		t.Fatalf("Invalid count of returned tasks, get: %d, want: %d",
 			len(tasks), len(planets.Self())*2)
 	}
