670fa2215caf1d311f47ca96885390fb21f2b9ba
hedhyw <hedhyw@yahoo.com>
2018-07-10 17:28:00 +0300

    Extend aiNames

--- Microservices/src/fakeserver/game/ainames.go
+++ Microservices/src/fakeserver/game/ainames.go
@@ -2,6 +2,8 @@ package game
 
 import (
 	"encoding/json"
+	"errors"
+	"math/rand"
 	"net/http"
 )
 
@@ -12,8 +14,13 @@ type aiNames struct {
 	Count int      `json:"count"`
 }
 
-// getAINames make a request to a aiservice for getting bot names
-func getAINames() ([]string, error) {
+// PickRandom returns a random AI name from the list
+func (aiNames *aiNames) PickRandom() string {
+	return aiNames.All[rand.Intn(len(aiNames.All))]
+}
+
+// newAiName make a request to a aiservice for getting bot names
+func newAiNames() (*aiNames, error) {
 	r, err := http.Get(aiNamesURL)
 	if err != nil {
 		return nil, err
@@ -24,5 +31,11 @@ func getAINames() ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
-	return names.All, nil
+	if names.Count != len(names.All) {
+		return nil, errors.New("aiservice returns invalid count of AI names")
+	}
+	if names.Count == 0 {
+		return nil, errors.New("aiservice doesn't have any AI")
+	}
+	return &names, nil
 }
