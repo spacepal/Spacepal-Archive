41420a544929ea1a918a8632b00f33533b2bc84e
hedhyw <hedhyw@yahoo.com>
2018-07-11 19:08:09 +0300

    Add pretty print

---
+++ Microservices/src/fakeserver/print.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fakeserver/game/igame"
+	"fmt"
+)
+
+func printPlayers(g igame.Game) {
+	players := g.Players()
+	fmt.Println("\n", color("=== Players ===").Blue())
+	fmt.Printf("% 10s : % 20s : % 8s : % 8s\n",
+		"ID", "Name", "Score", "Ships")
+	for _, p := range players {
+		fmt.Printf("% 10s : % 20s : % 8f : % 8d\n",
+			scolor("player$", p.ID()),
+			p.Name(),
+			p.Score(),
+			p.Ships(),
+		)
+		fmt.Println(scolor("player$", p.ID()), "\t", p.Name(), "\t", p.Score())
+	}
+}
