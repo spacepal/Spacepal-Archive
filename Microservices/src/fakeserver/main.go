fad136b71dc4766ea7c288e1cf325683bfc9b732
hedhyw <hedhyw@yahoo.com>
2018-07-11 18:27:53 +0300

    Create game proxy

--- Microservices/src/fakeserver/main.go
+++ Microservices/src/fakeserver/main.go
@@ -1,24 +1,111 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"fakeserver/game"
+	"fakeserver/game/igame"
 	"fakeserver/game/model"
+	"fmt"
 	"math/rand"
+	"net/http"
 	"time"
 
 	log "github.com/sirupsen/logrus"
 )
 
+const aiDoURL = "http://localhost:3131/ai/do"
+const servAddr = "localhost:3531"
+const callbackURL = "fake/callback?gameID=1"
+const timeoutDuration = 15 * time.Second
+
+type prox struct {
+	lock chan bool
+	game igame.Game
+	out  struct { // it's a buffer struct
+		Callback string         `json:"callback"`
+		Players  []igame.Player `json:"aiPlayers"`
+		MapSize  struct {
+			Width  int `json:"width"`
+			Height int `json:"height"`
+		} `json:"map"`
+		AllPlanets []igame.Planet `json:"planets"`
+	}
+	in struct {
+		Tasks []model.Task `json:"tasks"`
+	}
+}
+
+func newProx(game igame.Game, params model.GameParams) *prox {
+	var p = prox{}
+	p.lock = make(chan bool)
+	p.game = game
+	p.out.Callback = fmt.Sprintf("http://%s/%s", servAddr, callbackURL)
+	p.out.MapSize.Width = params.MapWidth
+	p.out.MapSize.Height = params.MapHeight
+
+	return &p
+}
+
+func (p *prox) StartServ() error {
+	http.HandleFunc("/", p.tasksHandle)
+	return http.ListenAndServe(servAddr, nil)
+}
+
+func (p *prox) tasksHandle(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	defer func() {
+		select {
+		case p.lock <- true:
+		default:
+		}
+	}()
+	if err := json.NewDecoder(r.Body).Decode(&p.in); err != nil {
+		p.in.Tasks = nil
+		log.Error(err)
+		return
+	}
+}
+
+func (p *prox) EndTurn() error {
+	raw, err := json.Marshal(p.out)
+	if err != nil {
+		return err
+	}
+	r, err := http.Post(aiDoURL, "application/json", bytes.NewBuffer(raw))
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+	if r.StatusCode != http.StatusOK {
+		return errors.New("Invalid http-status: " + r.Status)
+	}
+	select {
+	case <-p.lock:
+	case <-time.After(timeoutDuration):
+		return errors.New("Timeout")
+	}
+	p.game.EndTurn(p.in.Tasks)
+	return nil
+}
+
 func main() {
-	_, err := game.NewGame(model.GameParams{
+	var gameParams = model.GameParams{
 		MapHeight:    6,
 		MapWidth:     10,
 		PlayersCount: 4,
 		PlanetsCount: 20,
-	})
+	}
+	game, err := game.NewGame(gameParams)
 	if err != nil {
 		log.Fatal(err)
 	}
+	gameProxy := newProx(game, gameParams)
+	if gameProxy == nil {
+		log.Fatal("game proxy is nil")
+	}
+	go log.Error(gameProxy.StartServ())
 	log.Print("hello world")
 }
 
