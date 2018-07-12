b9500a12a7c2246d51b72446643b72d68778cc1d
hedhyw <hedhyw@yahoo.com>
2018-07-12 09:22:26 +0300

    Change NontleAI params

--- Microservices/src/aiservice/ai/list/all.go
+++ Microservices/src/aiservice/ai/list/all.go
@@ -10,23 +10,23 @@ import (
 func RegisterAll(r iai.ManagerRegister) {
 	r.Register("Nontle", ai.NewBase(
 		&factor{ // redistribution
-			quantity:    +0.2,
+			quantity:    +0.1,
 			random:      +4.0,
 			prod:        +1.0,
 			kill:        +0.0,
 			power:       +0.0,
-			ships:       +0.0,
+			ships:       0.0,
 			playerPower: +0.0,
-			distance:    -0.3,
+			distance:    -0.1,
 		},
 		&factor{ // attack
-			quantity:    +0.8,
-			random:      +3.0,
+			quantity:    +0.9,
+			random:      +1.0,
 			prod:        +0.6,
 			kill:        +0.4,
-			power:       +0.1,
-			ships:       +0.0,
+			power:       +0.0,
+			ships:       -0.2,
 			playerPower: +1.0,
-			distance:    -0.3,
+			distance:    -0.8,
 		}))
 }
