abf2a63408c1f144ab221dff8023ce4f83e2e047
hedhyw <hedhyw@yahoo.com>
2018-07-13 21:29:24 +0300

    Modify ActionCabel

--- Frontend/src/common/ActionCabel.js
+++ Frontend/src/common/ActionCabel.js
@@ -1,4 +1,4 @@
-import { GAME_CHANNEL } from './constants.js'
+import { GAME_CHANNEL, WS_SERVER } from './constants.js'
 import ActionCable from 'actioncable'
 import store from '@/store'
 
@@ -8,46 +8,58 @@ export default class ActionCabel {
   constructor (gameID) {
     this._okPromise = new Promise((resolve, reject) => {
       this._okResolvePromise = resolve
-      this._okRejectPromise = resolve
+      this._okRejectPromise = reject
     })
-    this._cable = ActionCable.createConsumer()
+    this._cable = ActionCable.createConsumer(WS_SERVER)
     this._cable.subscriptions.create({
       channel: GAME_CHANNEL,
-      room: `GAME:${gameID}`
+      room: `games:${gameID}`
     },
     {
-      connected: () => {
-        console.info('Action cable: connected')
-      },
-      received: data => {
-        console.info(`Action cable: data recieved`)
-        console.info(data)
-        if (data.type === 'error') {
-          this._okRejectPromise(false)
-        } else {
-          this._okResolvePromise(true)
-        }
-        if (data.type === 'players') {
-          store.dispatch('setMembers', data.data.players)
-        } else if (data.type === 'game') {
-          store.dispatch('game/setInfo', data.data)
-        } else if (data.type === 'profile') {
-          store.dispatch('setProfile', data.data)
-        } else if (data.type === 'fleets') {
-          store.dispatch('fleets/set', data.data.fleets)
-        } else if (data.type === 'fleets') {
-          store.dispatch('setPlanets', data.data.planets)
-        } else if (data.type === 'turn_ended') {
-          store.dispatch('reset')
-        } else {
-          console.warn(`ActionCable.js: Unknown type`)
-        }
-      },
-      disconnected: () => {
-        console.info('Action cable: disconnected')
-        this._okRejectPromise(false)
-      }
+      connected: this.onConnected,
+      received: this.onReceived,
+      disconnected: this.onDisconnected
     })
+  }
+  onConnected () {
+    console.info('Action cable: connected')
+  }
+  onDisconnected () {
+    console.info('Action cable: disconnected')
+    this._okRejectPromise(false)
+  }
+  onReceived (data) {
+    console.info(`Action cable: data recieved`)
+    console.info(data)
+    if (data.type === 'error') {
+      this._okRejectPromise(false)
+    } else {
+      this._okResolvePromise(true)
+    }
+    if (data.type === 'players') {
+      store.dispatch('setMembers', data.data.players)
+    } else if (data.type === 'game') {
+      store.dispatch('game/setInfo', data.data)
+    } else if (data.type === 'profile') {
+      this._cable.subscriptions.create({
+        channel: GAME_CHANNEL,
+        room: `players:${data.id}`
+      },
+      {
+        connected: this.onConnected,
+        received: this.onReceived,
+        disconnected: this.onDisconnected
+      })
+      store.dispatch('setProfile', data.data)
+    } else if (data.type === 'fleets') {
+      store.dispatch('fleets/set', data.data.fleets)
+    } else if (data.type === 'fleets') {
+      store.dispatch('setPlanets', data.data.planets)
+    } else if (data.type === 'turn_ended') {
+      store.dispatch('reset')
+    } else {
+      console.warn(`ActionCable.js: Unknown type`)
+    }
   }
   get isOk () {
     return this._okPromise
