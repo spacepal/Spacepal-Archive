c92fd75e76b9782680fa52c2d23ff67e9c000451
hedhyw <hedhyw@yahoo.com>
2018-07-14 23:38:34 +0300

    Waint after logout

--- Frontend/src/components/ActionButtons.vue
+++ Frontend/src/components/ActionButtons.vue
@@ -2,38 +2,44 @@
   <div class="active-buttons flex-horizontal">
     <template v-if="isAdmin && isRoom">
       <span class="button" @click="start">Start</span>
-      <span class="button" @click="regenerateMap">Regen</span>
+      <span class="button" @click="regenerateMap" :class="regenBtnClass">Regen</span>
     </template>
     <span class="button" v-if='!isLocked' @click="endTurn">End turn</span>
     <span class="button" @click="logout">
       <template v-if="isGame">
         Throw in the towel
       </template>
-      <template v-if="isRoom">
-        Logout
-      </template>
-      <template v-if="!isGame && !isRoom">
+      <template v-else>
         Exit
       </template>
     </span>
+    <FullPreloader ref="loader" />
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import FullPreloader from './FullPreloader'
 
 export default {
   name: 'ActionButtons',
+  components: {
+    FullPreloader
+  },
   computed: {
     ...mapGetters({
       profile: 'profile',
       isLoggedIn: 'isPlayer',
       isRoom: 'game/isRoom',
       isGame: 'game/isGame',
-      isLocked: 'isLocked'
+      isLocked: 'isLocked',
+      sync: 'sync'
     }),
     isAdmin () {
       return this.profile.isCreator
+    },
+    regenBtnClass () {
+      return this.sync.planets ? '' : 'disabled'
     }
   },
   methods: {
@@ -44,14 +50,19 @@ export default {
       this.$store.dispatch('game/endTurn')
     },
     regenerateMap () {
-      console.warn('ActionButtons.vue: @todo regenerateMap()')
+      if (!this.sync.planets || !this.$store.dispatch('shuffleMap')) {
+        this.$toast('Map isn\'t regenerated')
+      }
     },
     logout () {
-      this.$store.dispatch('logout').catch(err => {
+      this.$refs.loader.show()
+      this.$store.dispatch('logout').then(() => {
+        this.$router.push({
+          name: 'GamesList'
+        })
+      }).catch(err => {
+        this.$refs.loader.hide()
         this.$toast(err.message)
-      })
-      this.$router.push({
-        name: 'GamesList'
       })
     }
   }
