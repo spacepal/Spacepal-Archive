6a4e0b6cb755d266c24d7cd17d2dba9b3763255c
hedhyw <hedhyw@yahoo.com>
2018-07-19 21:34:22 +0300

    Create endpage

--- Frontend/src/pages/End.vue
+++ Frontend/src/pages/End.vue
@@ -1,9 +1,32 @@
 <template>
-  <div>End page</div>
+  <div>
+    <template v-if="sync.members">
+      <h1 v-if="isWinner">Congrulations!</h1>
+      <h1 v-else>You lose...</h1>
+    </template>
+    <template v-else>
+      Loading...
+    </template>
+    <Members></Members>
+    <div class="button" @click="exit">Exit</div>
+  </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex'
+import Members from '../components/Members'
 export default {
-  name: 'End'
+  name: 'End',
+  components: { Members },
+  methods: {
+    ...mapActions(['logout']),
+    exit () {
+      this.logout()
+      this.$router.push({ name: 'GamesList' })
+    }
+  },
+  computed: {
+    ...mapGetters(['isWinner', 'sync'])
+  }
 }
 </script>
