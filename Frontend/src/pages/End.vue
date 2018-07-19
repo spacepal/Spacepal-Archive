2720c38641b69a4c3d67a6b1aedabe5c1f25104d
hedhyw <hedhyw@yahoo.com>
2018-07-20 00:28:55 +0300

    Fix end page-proof

--- Frontend/src/pages/End.vue
+++ Frontend/src/pages/End.vue
@@ -1,23 +1,28 @@
 <template>
-  <div>
-    <template v-if="sync.members">
-      <h1 v-if="isWinner">Congrulations!</h1>
-      <h1 v-else>You lose...</h1>
-    </template>
-    <template v-else>
-      Loading...
-    </template>
-    <Members></Members>
-    <div class="button" @click="exit">Exit</div>
+  <div id="end_page">
+    <div>
+      <template v-if="sync.members">
+        <h1 v-if="isWinner">Congrulations!</h1>
+        <h1 v-else>You lose...</h1>
+      </template>
+      <template v-else>
+        <h1>
+          Loading...
+        </h1>
+      </template>
+      <Members></Members>
+      <div class="button" @click="exit">Exit</div>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
 import Members from '../components/Members'
+import Form from '../components/Form'
 export default {
   name: 'End',
-  components: { Members },
+  components: { Members, Form },
   methods: {
     ...mapActions(['logout']),
     exit () {
@@ -30,3 +35,17 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+#end_page {
+  position: absolute;
+  left: 0;
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100%;
+  min-width: 100%;
+  text-align: center;
+}
+</style>
