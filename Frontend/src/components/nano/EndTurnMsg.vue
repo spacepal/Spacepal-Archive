0a29a0d36d3d3d28f87f999547dab61ba033378e
hedhyw <hedhyw@yahoo.com>
2018-07-20 09:24:56 +0300

    Add EndTurnMsg

---
+++ Frontend/src/components/nano/EndTurnMsg.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="turn-number-bg" v-show="isShowed">
+    <div class="circle">
+      <div class="turn-number">{{turn}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EndTurnMsg',
+  data () {
+    return {
+      isShowed: false,
+      turn: 0
+    }
+  },
+  methods: {
+    show (turn) {
+      clearTimeout(this._timer)
+      this._timer = setTimeout(() => {
+        this.isShowed = false
+      }, 600)
+      this.isShowed = true
+      if (turn > 9999) {
+        this.turn = Math.ceil(turn / 10000)
+        this.turn += 'k'
+      } else {
+        this.turn = turn
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../css/components/_end_turn_msg.scss';
+</style>
